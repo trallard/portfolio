f6f2d6b3ae32d294018ee6253ca5bc21a2aa8c02
Tania Allard <taniar.allard@gmail.com>
2020-04-07 10:14:54 +0100

    :bulb: Update Readme

--- src/components/email.js
+++ src/components/email.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { email } from '@config';
+
 import { Side } from '@components';
 import styled from 'styled-components';
 import { theme } from '@styles';
